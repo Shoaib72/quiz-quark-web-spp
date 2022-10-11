8af20b7603f2456d1b8a01e59285b1ac9b7e345e
Shoaib72 <shoaib.953872@gmail.com>
2022-10-11 07:05:55 +0100

    nav-styled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className='w-full flex'>
+        <div className='w-full h-20 border-stone-500 rounded-lg flex justify-around items-center bg-gray-200'>
             <div>
-                <h1 className='text-4xl font-bold'>Quiz<span className='text-orange-600'>Q</span>uark</h1>
+                <h1 className='text-5xl font-bold'>Quiz<span className='text-orange-600'>Q</span>uark</h1>
             </div>
             <nav>
-                <ul>
+                <ul className='px-3 py-3 lg:flex md:flex gap-10 text-orange-600 font-bold'>
                     <li><Link to="/home">Topics</Link></li>
                     <li><Link to="/statistics">Statistics</Link></li>
                     <li><Link to="/blog">Blog</Link></li>
