42fa033686fed8cd998d2abfd991f3b97a4aea6f
Shoaib72 <shoaib.953872@gmail.com>
2022-10-12 00:53:08 +0100

    correct-eye-added

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Questions = ({ quizQuestion }) => {
     const options = quizQuestion.options;
     console.log(quizQuestion)
     return (
         <div>
-            <h1 className='text-2xl text-bold mt-8 sm:ml=5 sm:mr-5'>{quizQuestion.question}</h1>
+            <div className='flex flex-col justify-center items-center'>
+                <h1 className='text-2xl text-bold mt-8'>{quizQuestion.question}</h1>
+                <EyeIcon className='h-5 w-5 mt-5 hover:bg-red-600'></EyeIcon>
+            </div>
             {
-                options.map(option => <div className='border-2 md:w-1/2 ml-10 mr-20 md:ml-80 mt-5 rounded-lg p-8 hover:bg-slate-500 text-xl text-bold mb-10'>{option}</div>)
+                options.map(option => <div className='border-2 md:w-1/2 ml-10 mr-20 md:ml-80 mt-5 rounded-lg p-8 hover:bg-slate-500 text-xl text-bold mb-10'>{option}</div>
+                )
             }
         </div>
     );
