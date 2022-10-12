a44241c4fca60123c9b1e4a56134a202399dff7a
Shoaib72 <shoaib.953872@gmail.com>
2022-10-12 02:14:49 +0100

    toast-edded

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,19 +1,63 @@
 import React from 'react';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Questions = ({ quizQuestion }) => {
     const options = quizQuestion.options;
-    console.log(quizQuestion)
+    // console.log(quizQuestion)
+    const correctAnswerCheck = (text) => {
+        const correctAnswer = quizQuestion.correctAnswer;
+        if (text === correctAnswer) {
+            toast.success('😇 Well Done! Correct Answer!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+        // note:This is special else if condition for first question of React quiz. All the
+        // quiz question were giving the right result but react first question wasn't giving the right toast even though user is giving the exact right answer. Thats why i have put this special "else if" condition manually only for React's 1st Quiz Question.
+        else if (text === 'building User Interface') {
+            toast.success('😇 Well Done! Correct Answer!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+        else {
+            toast.error('😬Wrong Answer! Try Again!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+    }
     return (
         <div>
             <div className='flex flex-col justify-center items-center'>
                 <h1 className='text-2xl text-bold mt-8'>{quizQuestion.question}</h1>
-                <EyeIcon className='h-5 w-5 mt-5 hover:bg-red-600'></EyeIcon>
+                <EyeIcon className='h-5 w-5 mt-5 hover:bg-red-500'></EyeIcon>
             </div>
             {
-                options.map(option => <div className='border-2 md:w-1/2 ml-10 mr-20 md:ml-80 mt-5 rounded-lg p-8 hover:bg-slate-500 text-xl text-bold mb-10'>{option}</div>
+                options.map(option => <div onClick={(e) => correctAnswerCheck(e.target.innerText)} className='border-2 md:w-1/2 ml-10 mr-20 md:ml-80 mt-5 rounded-lg p-8 hover:bg-slate-500 text-xl text-bold mb-10'>{option}</div>
                 )
             }
+            <ToastContainer />
         </div>
     );
 };
